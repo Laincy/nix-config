2548eb436b0ebd2662dcce03beefad3a3354d06d
Laincy <laincy@proton.me>
2025-05-07 23:55:03 -0400

    move notification location in Hyprpanel

--- home/desktop/panel.nix
+++ home/desktop/panel.nix
@@ -16,6 +16,7 @@
 
     settings = {
       terminal = "alacritty";
+			notifications.position = "bottom right";
       layout."bar.layouts"."0" = {
         left = ["dashboard" "workspaces"];
         middle = ["media"];
